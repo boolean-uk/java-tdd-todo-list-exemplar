c56fdcaec43baf4fe07d32a5879da2380da6a68c
Dave Ames <david.john.ames@gmail.com>
2023-03-23 10:37:53 +0000

    Implemented tests for and solutions to the Core requirements.

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,120 @@
 package com.booleanuk.core;
 
-public class TodoList {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 
+// Could fairly easily refactor some of these methods so that they called a single method passing it a context to
+// do things such as return all/complete/incomplete tasks and return the tasks in ascending/descending order etc.
+
+public class TodoList {
+    HashMap<String, String[]> taskList;
+
+    public TodoList() {
+        this.taskList = new HashMap<>();
+    }
+
+    public boolean add(String title, String taskDetails) {
+        if (this.taskList.containsKey(title)) {
+            return false;
+        } else {
+            this.taskList.put(title, new String[] {taskDetails, "Incomplete"});
+            return true;
+        }
+    }
+
+    public String displayAllTasks() {
+        String result = "";
+        for (HashMap.Entry<String, String[]> entry : this.taskList.entrySet()) {
+            String key = entry.getKey();
+            String[] item = entry.getValue();
+            result += "Title: " + key + " Task: " + item[0] + " Status: " + item[1] +"\n";
+        }
+        return result;
+    }
+
+    public String toggleStatus(String title) {
+        if (this.taskList.containsKey(title)) {
+            String[] contents = this.taskList.get(title);
+            if (contents[1].equals("Complete")) {
+                contents[1] = "Incomplete";
+            } else {
+                contents[1] = "Complete";
+            }
+            this.taskList.put(title, contents);
+            return "Success";
+        } else {
+            return "Failure";
+        }
+    }
+
+    public String displayCompletedTasks() {
+        String result = "";
+        for (HashMap.Entry<String, String[]> entry : this.taskList.entrySet()) {
+            String key = entry.getKey();
+            String[] item = entry.getValue();
+            if (item[1].equals("Complete")) {
+                result += "Title: " + key + " Task: " + item[0] + " Status: " + item[1] + "\n";
+            }
+        }
+        return result;
+    }
+
+    public String displayIncompleteTasks() {
+        String result = "";
+        for (HashMap.Entry<String, String[]> entry : this.taskList.entrySet()) {
+            String key = entry.getKey();
+            String[] item = entry.getValue();
+            if (item[1].equals("Incomplete")) {
+                result += "Title: " + key + " Task: " + item[0] + " Status: " + item[1] + "\n";
+            }
+        }
+        return result;
+    }
+
+    public String getTaskByName(String title) {
+        String result = "";
+        if (this.taskList.containsKey(title))
+        {
+            String[] contents = this.taskList.get(title);
+            result = "Title: " + title + " Task: " + contents[0] + " Status: " + contents[1];
+        } else {
+            result = "I'm sorry we couldn't find that item.";
+        }
+        return result;
+    }
+
+    public String remove(String title) {
+        String result = "";
+        if (this.taskList.containsKey(title)) {
+            this.taskList.remove(title);
+            result = "Successfully removed task.";
+        } else {
+            result = "Could not find the task specified.";
+        }
+        return result;
+    }
+
+    public String displayAllTasksAscending() {
+        ArrayList<String> theKeys = new ArrayList<>(this.taskList.keySet());
+        Collections.sort(theKeys);
+        String result = "";
+        for (String key : theKeys) {
+            String[] contents = this.taskList.get(key);
+            result += "Title: " + key + " Task: " + contents[0] + " Status: " + contents[1] +"\n";
+        }
+        return result;
+    }
+
+    public String displayAllTasksDescending() {
+        ArrayList<String> theKeys = new ArrayList<>(this.taskList.keySet());
+        Collections.sort(theKeys);
+        Collections.reverse(theKeys);
+        String result = "";
+        for (String key : theKeys) {
+            String[] contents = this.taskList.get(key);
+            result += "Title: " + key + " Task: " + contents[0] + " Status: " + contents[1] +"\n";
+        }
+        return result;
+    }
 }
--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -5,9 +5,183 @@ import org.junit.jupiter.api.Test;
 
 class TodoListTest {
     @Test
-    public void exampleTest() {
-        String hello = "Hello";
-        Assertions.assertEquals("Hello", hello);
-        Assertions.assertNotEquals("Goodbye", hello);
+    public void addTaskSucceeds() {
+        TodoList myList = new TodoList();
+        boolean result = myList.add("Tidy room", "Tidy up all of the stuff in the room.");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void addTaskFails() {
+        // Adding a task should fail if the title is a duplicate.
+        TodoList myList = new TodoList();
+        myList.add("Tidy room", "Tidy up all of the stuff in the room.");
+        boolean result = myList.add("Tidy room", "Tidy up all of the stuff in the room.");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void displayAllTasksCorrectly() {
+        TodoList myList = new TodoList();
+        myList.add("Tidy room", "Tidy up all of the stuff in the room.");
+        myList.add("Do washing up", "Wash up all dirty dishes etc.");
+        myList.add("Wash clothes", "Do all of the laundry.");
+
+        String result = "Title: Tidy room Task: Tidy up all of the stuff in the room. Status: Incomplete\n";
+        result += "Title: Do washing up Task: Wash up all dirty dishes etc. Status: Incomplete\n";
+        result += "Title: Wash clothes Task: Do all of the laundry. Status: Incomplete\n";
+
+        Assertions.assertEquals(result, myList.displayAllTasks());
+    }
+
+    @Test
+    public void checkForTaskCompletedAfterToggle() {
+        TodoList myList = new TodoList();
+        myList.add("Tidy room", "Tidy up all of the stuff in the room.");
+        myList.add("Do washing up", "Wash up all dirty dishes etc.");
+
+        String result = myList.toggleStatus("Tidy room");
+        Assertions.assertEquals("Success", result);
+        String outcome = "Title: Tidy room Task: Tidy up all of the stuff in the room. Status: Complete\n";
+        outcome += "Title: Do washing up Task: Wash up all dirty dishes etc. Status: Incomplete\n";
+        Assertions.assertEquals(outcome, myList.displayAllTasks());
+    }
+
+    @Test
+    public void checkForTaskIncompleteAfterToggle() {
+        TodoList myList = new TodoList();
+        myList.add("Tidy room", "Tidy up all of the stuff in the room.");
+        myList.add("Do washing up", "Wash up all dirty dishes etc.");
+        myList.toggleStatus("Tidy room");
+        String result = myList.toggleStatus("Tidy room");
+        Assertions.assertEquals("Success", result);
+        String outcome = "Title: Tidy room Task: Tidy up all of the stuff in the room. Status: Incomplete\n";
+        outcome += "Title: Do washing up Task: Wash up all dirty dishes etc. Status: Incomplete\n";
+        Assertions.assertEquals(outcome, myList.displayAllTasks());
+    }
+
+    @Test
+    public void checkForFailureOnToggle() {
+        TodoList myList = new TodoList();
+        myList.add("Tidy room", "Tidy up all of the stuff in the room.");
+        myList.add("Do washing up", "Wash up all dirty dishes etc.");
+
+        String result = myList.toggleStatus("Tidy");
+        Assertions.assertEquals("Failure", result);
+        String outcome = "Title: Tidy room Task: Tidy up all of the stuff in the room. Status: Incomplete\n";
+        outcome += "Title: Do washing up Task: Wash up all dirty dishes etc. Status: Incomplete\n";
+        Assertions.assertEquals(outcome, myList.displayAllTasks());
+    }
+
+    @Test
+    public void displayCompletedTasksCorrectly() {
+        TodoList myList = new TodoList();
+        myList.add("Tidy room", "Tidy up all of the stuff in the room.");
+        myList.add("Do washing up", "Wash up all dirty dishes etc.");
+        myList.add("Wash clothes", "Do all of the laundry.");
+
+        myList.toggleStatus("Tidy room");
+        myList.toggleStatus("Wash clothes");
+
+        String result = "Title: Tidy room Task: Tidy up all of the stuff in the room. Status: Complete\n";
+        result += "Title: Wash clothes Task: Do all of the laundry. Status: Complete\n";
+
+        Assertions.assertEquals(result, myList.displayCompletedTasks());
+    }
+
+    @Test
+    public void displayIncompleteTasksCorrectly() {
+        TodoList myList = new TodoList();
+        myList.add("Tidy room", "Tidy up all of the stuff in the room.");
+        myList.add("Do washing up", "Wash up all dirty dishes etc.");
+        myList.add("Wash clothes", "Do all of the laundry.");
+
+        myList.toggleStatus("Tidy room");
+        myList.toggleStatus("Wash clothes");
+
+        String result = "Title: Do washing up Task: Wash up all dirty dishes etc. Status: Incomplete\n";
+
+        Assertions.assertEquals(result, myList.displayIncompleteTasks());
+    }
+
+    @Test
+    public void displayTaskByTitle() {
+        TodoList myList = new TodoList();
+        myList.add("Tidy room", "Tidy up all of the stuff in the room.");
+        myList.add("Do washing up", "Wash up all dirty dishes etc.");
+        myList.add("Wash clothes", "Do all of the laundry.");
+
+        String result = "Title: Do washing up Task: Wash up all dirty dishes etc. Status: Incomplete";
+
+        Assertions.assertEquals(result, myList.getTaskByName("Do washing up"));
+    }
+
+    @Test
+    public void displayTaskByTitleFailureMessage() {
+        TodoList myList = new TodoList();
+        myList.add("Tidy room", "Tidy up all of the stuff in the room.");
+        myList.add("Do washing up", "Wash up all dirty dishes etc.");
+        myList.add("Wash clothes", "Do all of the laundry.");
+
+        String result = "I'm sorry we couldn't find that item.";
+
+        Assertions.assertEquals(result, myList.getTaskByName("Wash up"));
+    }
+
+    @Test
+    public void removeTaskFromList() {
+        TodoList myList = new TodoList();
+        myList.add("Tidy room", "Tidy up all of the stuff in the room.");
+        myList.add("Do washing up", "Wash up all dirty dishes etc.");
+        myList.add("Wash clothes", "Do all of the laundry.");
+
+        String taskList = "Title: Do washing up Task: Wash up all dirty dishes etc. Status: Incomplete\n";
+        taskList += "Title: Wash clothes Task: Do all of the laundry. Status: Incomplete\n";
+
+        Assertions.assertEquals("Successfully removed task.", myList.remove("Tidy room"));
+        Assertions.assertEquals(taskList, myList.displayAllTasks());
+    }
+
+    @Test
+    public void failToRemoveTaskFromList() {
+        TodoList myList = new TodoList();
+        myList.add("Tidy room", "Tidy up all of the stuff in the room.");
+        myList.add("Do washing up", "Wash up all dirty dishes etc.");
+        myList.add("Wash clothes", "Do all of the laundry.");
+
+        String taskList = "Title: Tidy room Task: Tidy up all of the stuff in the room. Status: Incomplete\n";
+        taskList += "Title: Do washing up Task: Wash up all dirty dishes etc. Status: Incomplete\n";
+        taskList += "Title: Wash clothes Task: Do all of the laundry. Status: Incomplete\n";
+
+        Assertions.assertEquals("Could not find the task specified.", myList.remove("Tidy bedroom"));
+        Assertions.assertEquals(taskList, myList.displayAllTasks());
+    }
+
+    @Test
+    public void checkSeeTasksInAscendingOrder() {
+        TodoList myList = new TodoList();
+        myList.add("Tidy room", "Tidy up all of the stuff in the room.");
+        myList.add("Do washing up", "Wash up all dirty dishes etc.");
+        myList.add("Wash clothes", "Do all of the laundry.");
+
+        String sortedTaskList = "Title: Do washing up Task: Wash up all dirty dishes etc. Status: Incomplete\n";
+        sortedTaskList += "Title: Tidy room Task: Tidy up all of the stuff in the room. Status: Incomplete\n";
+        sortedTaskList += "Title: Wash clothes Task: Do all of the laundry. Status: Incomplete\n";
+
+        Assertions.assertEquals(sortedTaskList, myList.displayAllTasksAscending());
+    }
+
+    @Test
+    public void checkSeeTasksInDescendingOrder() {
+        TodoList myList = new TodoList();
+        myList.add("Tidy room", "Tidy up all of the stuff in the room.");
+        myList.add("Do washing up", "Wash up all dirty dishes etc.");
+        myList.add("Wash clothes", "Do all of the laundry.");
+
+        String sortedTaskList = "Title: Wash clothes Task: Do all of the laundry. Status: Incomplete\n";
+        sortedTaskList += "Title: Tidy room Task: Tidy up all of the stuff in the room. Status: Incomplete\n";
+        sortedTaskList += "Title: Do washing up Task: Wash up all dirty dishes etc. Status: Incomplete\n";
+
+        Assertions.assertEquals(sortedTaskList, myList.displayAllTasksDescending());
     }
 }
